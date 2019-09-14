b4e41fabe537a93517c6ee8cb732995a6e936f90
saimanish <manish.naganuri1@gmail.com>
2019-09-14 05:50:42 +0530

    Create transaction pool

---
+++ wallet/transaction-pool.js
@@ -0,0 +1,10 @@
+class TransactionPool {
+  constructor() {
+    this.transactionMap = {};
+  }
+  setTransaction(transaction) {
+    this.transactionMap[transaction.id] = transaction;
+  }
+}
+
+module.exports = TransactionPool;
